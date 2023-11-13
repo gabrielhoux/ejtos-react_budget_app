7a4781e137eb7d258f3fba20100ad7291637d763
gabrielhoux <gabriel.houx@gmail.com>
2023-11-13 16:09:14 +0100

    completed Budget Allocation app

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency';
 
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
+
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
                     <div className='col-sm'>
@@ -23,13 +25,18 @@ const App = () => {
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
+                    <div className='col-sm'>
+                        <Currency />
+                    </div>   
                 </div>
+
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
                 </div>
+
                 <h3 className='mt-3'>Change allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,29 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        const updatedBudget = event.target.value;
+
+        // Alert if budget is set lower than current spending
+        if (parseInt(updatedBudget) < totalExpenses) {
+            alert("You cannot reduce the budget lower than the spending");
+            return;
+        }
+
+        setNewBudget(updatedBudget);
+        // Update budget in the context using the dispatch function
+        dispatch({ type: 'SET_BUDGET', payload: updatedBudget });
     }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+            <span>Budget: {currency}</span> {/* Display the currency symbol */}
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange} />
         </div>
     );
 };
